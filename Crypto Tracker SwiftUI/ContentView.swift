7d7d07f4eab7f85f198b0cfb40db606b3590e264
Dalveer Singh <59986053+singhbaidwan@users.noreply.github.com>
2022-12-22 16:37:48 +0530

    Initial Setup

--- Crypto Tracker SwiftUI/ContentView.swift
+++ Crypto Tracker SwiftUI/ContentView.swift
@@ -10,10 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Color.theme.secondaryText
+                .ignoresSafeArea(.all)
         }
         .padding()
     }
---
+++ Crypto Tracker SwiftUI/Extensions/Colors.swift
@@ -0,0 +1,22 @@
+//
+//  Colors.swift
+//  Crypto Tracker SwiftUI
+//
+//  Created by Dalveer singh on 22/12/22.
+//
+
+import SwiftUI
+
+extension Color{
+    static let theme = ColorTheme()
+    
+}
+
+
+struct ColorTheme{
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
