732e20962a24a3119516166f8aad85670546a7da
Dalveer Singh <59986053+singhbaidwan@users.noreply.github.com>
2022-12-22 16:28:05 +0530

    Initial Commit

---
+++ Crypto Tracker SwiftUI/Crypto_Tracker_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_Tracker_SwiftUIApp.swift
+//  Crypto Tracker SwiftUI
+//
+//  Created by Dalveer singh on 22/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_Tracker_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
